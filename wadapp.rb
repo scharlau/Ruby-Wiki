cc6d9fb6e3774f0e2908d2dc323f94a7a53e889f
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2018-09-24 19:57:20 +0100

    added edit form and method

--- wadapp.rb
+++ wadapp.rb
@@ -3,14 +3,34 @@ require 'sinatra'
 $myinfo = "firstname lastname"
 @info = "" 
 
+def readFile(filename)
+    info = ""
+    file = File.open(filename)
+    file.each do |line|
+        info = info + line
+    end
+    file.close
+    $myinfo = info
+end
+
 #root landing page
 get '/' do
     info = "Hello Ruby!"
+
+    len = info.length
+    len1 = len
+    readFile("wiki.txt")
     @info = info + " " + $myinfo
+    len = @info.length
+    len2 = len-1
+    len3 = len2-len1
+    @info = @info +  " " + len3.to_s
     '<html><body>' +
     '<b>Menu</b><br>' +
+    '<a href="/">Home</a><br>' +
     '<a href="/create">Create</a><br>' +
     '<a href="/about">About</a><br>' +
+    '<a href="/edit">Edit</a><br>' + 
     '<br><br>' + @info +
     '</body></html>'
 end
@@ -30,7 +50,6 @@ get '/about' do
 end
 
 #create page
-
 get '/create' do
     '<html><body>
     <b>Menu</b><br>
@@ -40,6 +59,35 @@ get '/create' do
     '<h2>This is your own create page</h2>
     <section id="add">' + $myinfo + '</section>' +
     '</body></html>'
+end
+
+#edit form page
+get '/edit' do
+    info = ""
+    file = File.open("wiki.txt")
+    file.each do |line|
+        info = info + line
+    end
+    file.close
+    @info = info
+    '<h2>Edit article</h2><br>' +
+    '<form action= "/edit" method="post" id="edit">
+    <input type ="hidden" name="_method" value="put">
+        <textarea rows="20" cols="90" name="message">' + @info + '</textarea>
+        <input type = "submit" value = "Update">
+    </form>' +
+    '<p><a href="../reset>"Reset</a></p>'
+end
+
+#put method to update wiki.txt file from form page
+put '/edit' do
+    info = "#{params[:message]}"
+    @info = info
+    puts @info
+    file = File.open("wiki.txt", "w")
+    file.puts @info
+    file.close
+    redirect '/'
 end
 
 #for page not found issues
