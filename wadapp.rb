1dca4b56a70e6611cbeeb6c50cced734d74a474f
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2018-09-24 18:13:10 +0100

    added about page

--- wadapp.rb
+++ wadapp.rb
@@ -15,20 +15,22 @@ get '/' do
     '</body></html>'
 end
 
-#for page not found issues
-not_found do
-    status 404
-    redirect '/'
-end
 
 #about page
 get '/about' do
     '<html><body>
     <b>Menu</b><br>
+    <a href="/">Home</a><br>
     <a href="/create">Create</a><br>
-    <a href="/about">About</a><br>' +
-    '<h2>About us</h2>'
-    '<p>This wiki was created by</p>' + $myinfo + 
-    '<p>Staff ID xxxxxx</p>' +
-    '</body></html>'
+    <a href="/about">About</a><br>' + 
+    '<h2>About us</h2>
+    <p>This wiki was created by</p>' + $myinfo + 
+    '<p>Staff ID xxxxxx</p> 
+    </body></html>'
+end
+
+#for page not found issues
+not_found do
+    status 404
+    redirect '/'
 end
