52a071014b69a2c8ed2040ed282622d451225dfe
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2018-09-24 17:37:39 +0100

    added about page

--- wadapp.rb
+++ wadapp.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 $myinfo = "firstname lastname"
 @info = "" 
 
+#root landing page
 get '/' do
     info = "Hello Ruby!"
     @info = info + " " + $myinfo
@@ -11,5 +12,23 @@ get '/' do
     '<a href="/create">Create</a><br>' +
     '<a href="/about">About</a><br>' +
     '<br><br>' + @info +
+    '</body></html>'
+end
+
+#for page not found issues
+not_found do
+    status 404
+    redirect '/'
+end
+
+#about page
+get '/about' do
+    '<html><body>
+    <b>Menu</b><br>
+    <a href="/create">Create</a><br>
+    <a href="/about">About</a><br>' +
+    '<h2>About us</h2>'
+    '<p>This wiki was created by</p>' + $myinfo + 
+    '<p>Staff ID xxxxxx</p>' +
     '</body></html>'
 end
